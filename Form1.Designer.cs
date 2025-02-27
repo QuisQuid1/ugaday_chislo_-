12c56e78db0d37fe98f2089d78197187e84c5fbd
QuisQuid1 <marunenkod@bk.ru>
2025-02-27 13:17:51 +0500

    добавил метод запуска игры

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_povtor = new System.Windows.Forms.Button();
+            this.button_check = new System.Windows.Forms.Button();
+            this.label_rez = new System.Windows.Forms.Label();
+            this.textBox_chisl = new System.Windows.Forms.TextBox();
+            this.label_pop = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_povtor
+            // 
+            this.button_povtor.Location = new System.Drawing.Point(478, 336);
+            this.button_povtor.Name = "button_povtor";
+            this.button_povtor.Size = new System.Drawing.Size(75, 42);
+            this.button_povtor.TabIndex = 9;
+            this.button_povtor.Text = "button1";
+            this.button_povtor.UseVisualStyleBackColor = true;
+            // 
+            // button_check
+            // 
+            this.button_check.Location = new System.Drawing.Point(107, 336);
+            this.button_check.Name = "button_check";
+            this.button_check.Size = new System.Drawing.Size(75, 42);
+            this.button_check.TabIndex = 8;
+            this.button_check.Text = "button1";
+            this.button_check.UseVisualStyleBackColor = true;
+            // 
+            // label_rez
+            // 
+            this.label_rez.AutoSize = true;
+            this.label_rez.Location = new System.Drawing.Point(192, 233);
+            this.label_rez.Name = "label_rez";
+            this.label_rez.Size = new System.Drawing.Size(51, 20);
+            this.label_rez.TabIndex = 7;
+            this.label_rez.Text = "label1";
+            // 
+            // textBox_chisl
+            // 
+            this.textBox_chisl.Location = new System.Drawing.Point(68, 117);
+            this.textBox_chisl.Name = "textBox_chisl";
+            this.textBox_chisl.Size = new System.Drawing.Size(100, 26);
+            this.textBox_chisl.TabIndex = 6;
+            // 
+            // label_pop
+            // 
+            this.label_pop.AutoSize = true;
+            this.label_pop.Location = new System.Drawing.Point(64, 40);
+            this.label_pop.Name = "label_pop";
+            this.label_pop.Size = new System.Drawing.Size(51, 20);
+            this.label_pop.TabIndex = 5;
+            this.label_pop.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_povtor);
+            this.Controls.Add(this.button_check);
+            this.Controls.Add(this.label_rez);
+            this.Controls.Add(this.textBox_chisl);
+            this.Controls.Add(this.label_pop);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_povtor;
+        private System.Windows.Forms.Button button_check;
+        private System.Windows.Forms.Label label_rez;
+        private System.Windows.Forms.TextBox textBox_chisl;
+        private System.Windows.Forms.Label label_pop;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,21 @@ namespace ugaday_chislo__
 {
     public partial class Form1 : Form
     {
+        private int targetNumber;
+        private int attempts;
         public Form1()
         {
             InitializeComponent();
+            StartNewGame();
+        }
+        private void StartNewGame()
+        {
+            Random random = new Random();
+            targetNumber = random.Next(1, 101); // Случайное число от 1 до 100
+            attempts = 0;
+            label_rez.Text = "Попробуйте угадать число от 1 до 100!";
+            label_pop.Text = "Попытки: 0";
+            textBox_chisl.Text = "";
         }
     }
 }
